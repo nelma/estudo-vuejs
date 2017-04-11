2517ce8da9d6940b03d9252178304c14b7fc6a00
Nelma <nelmasb@gmail.com>
2017-04-10 22:36:15 -0300

    usando promise

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -3,7 +3,9 @@
     <h1>{{ titulo }}</h1>
     
     <ul>
-      <li><img v-bind:src="foto.url" v-bind:alt="foto.titulo"></li>
+      <li v-for="foto of fotos">
+        <img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+      </li>
     </ul>
      
   </div>
@@ -15,17 +17,16 @@ export default {
     return{
       
       titulo: 'Alurapic',
-      fotos: [
-        {
-          url: 'http://www.farejadordecaes.com.br/wp-content/uploads/o-que-saber-antes-de-comprar-cachorro-01.png',
-          titulo: 'Cachorro'
-        },
-        {
-          url: 'http://www.farejadordecaes.com.br/wp-content/uploads/o-que-saber- antes-de-comprar-cachorro-01.png',
-          titulo: 'Gato'
-        }
-      ] 
+      fotos: [] 
     }
+  },
+  created() {
+    //alert("Criei o componente");
+
+    let promise = this.$http.get('http://localhost:3000/v1/fotos');
+    promise
+      .then(res => res.json())
+      .then(fotinhos => this.fotos = fotinhos, err => console.log(err));
   }
 }
 </script>
