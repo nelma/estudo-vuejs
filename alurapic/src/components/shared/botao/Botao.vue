cbd1cf3ba28025ae1b931be023cc2ad1b33e857e
Nelma <nelmasb@gmail.com>
2017-04-21 07:23:07 -0300

    validando props

--- alurapic/src/components/shared/botao/Botao.vue
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -1,15 +1,46 @@
 <template>
-    <button class="botao botao-perigo" :type="tipo" @click="disparaAcao()">{{ rotulo }}</button>
+    <button class="botao" :class="estiloDoBotao" :type="tipo" @click="disparaAcao()">{{ rotulo }}</button>
 </template>
 
 <script>
     export default {
-        props: ['tipo', 'rotulo'],
+        //props: ['tipo', 'rotulo', 'confirmacao', 'estilo'],
+
+        props: {
+            tipo: {
+                type: String,
+                required: true
+            },
+            rotulo: {
+                type: String,
+                required: true
+            },
+            confirmacao: Boolean,
+            estilo: String
+        },
 
         methods: {
             disparaAcao() {
-                if(confirm('Confirma operação?')) {
-                    this.$emit('botaoAtivado');
+
+                if(this.confirmacao) {
+                    if(confirm('Confirma operação?')) {
+                        this.$emit('botaoAtivado');
+                    }
+                    return;
+                }
+                this.$emit('botaoAtivado');
+            }
+        },
+
+        computed: {
+            estiloDoBotao() {
+                //se valor padrao ou não passou nada para estilo
+                if(this.estilo == 'padrao' || !this.estilo) {
+                    return 'botao botao-padrao';
+                }
+
+                if(this.estilo == 'perigo') {
+                    return 'botao botao-perigo';
                 }
             }
         }
