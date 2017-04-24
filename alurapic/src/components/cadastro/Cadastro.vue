e483ac04b9fa22427dbab6f04afcaf99d60e1db9
nbarros@uolinc.com <nbarros@uolinc.com>
2017-04-24 09:52:56 -0300

    capturando dados no form

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -1,15 +1,94 @@
 <template>
-    <div>
-        <h1 class="centralizado">Cadatro</h1>
-    </div>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input id="titulo" autocomplete="off" :value="foto.titulo" @input="foto.titulo = $event.target.value">
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input id="url" autocomplete="off" :value="foto.url" @input="foto.url = $event.target.value">
+        <imagem-responsiva/>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" :value="foto.descricao" @input="foto.descricao = $event.target.value"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link to="/"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
 
 <script>
-    export default {}
-</script>
 
-<style scoped>
-    .centralizado {
-        text-align: center;
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+
+export default {
+
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva,
+    'meu-botao': Botao
+  },
+
+  methods: {
+    grava() {
+      console.log(this.foto);
+      this.foto = {
+          titulo: '',
+          url: '',
+          descricao: ''
+      };
     }
+  },
+
+  data() {
+    return {
+      foto: {
+        titulo: '',
+        url: '',
+        descricao: ''
+      }
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
 </style>
