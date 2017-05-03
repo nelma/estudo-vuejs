183bc0a060f88b0b1f5269aa9a0c533294fc4348
Nelma <nelmasb@gmail.com>
2017-05-03 06:54:32 -0300

    estilizando com SASS e separando arquivos de estilo

--- alurapic/src/components/shared/botao/Botao.vue
+++ alurapic/src/components/shared/botao/Botao.vue
@@ -47,24 +47,8 @@
     }
 </script>
 
-<style scoped>
-    .botao {
-        display: inline-block;
-        padding: 10px;
-        border-radius: 3px;
-        margin: 10px;
-        font-size: 1.2em;
-    }
-
-    .botao-perigo {
-        background: firebrick;
-        color: white;
-    }
-
-    .botao-padrao {
-        background: darkcyan;
-        color: white;
-    }
+<style scoped lang="scss">
+    @import './Botao.scss';
 </style>
 
 
